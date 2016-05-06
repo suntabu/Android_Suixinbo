618a00f2b4f252267dc1dbaafec7e8d8a1712d9b
xkazerzhang <xkazerzhang@tencent.com>
2016-05-06 19:21:22 +0800

    完善直播列表的显示信息

--- app/src/main/java/com/tencent/qcloud/suixinbo/adapters/LiveShowAdapter.java
+++ app/src/main/java/com/tencent/qcloud/suixinbo/adapters/LiveShowAdapter.java
@@ -29,18 +29,15 @@ import java.util.ArrayList;
 public class LiveShowAdapter extends ArrayAdapter<LiveInfoJson> {
     private static String TAG = "LiveShowAdapter";
     private int resourceId;
-    private View view;
-//    private ImageLoader imageLoader = ImageLoader.getInstance();
     private Activity mActivity;
-
-//    private DisplayImageOptions options = new DisplayImageOptions.Builder()
-//            .cacheInMemory(true)
-//            .resetViewBeforeLoading(true)
-//            .showImageOnLoading(R.drawable.cover_background)
-//            .showImageOnFail(R.drawable.cover_background)
-//            .cacheOnDisk(true)
-//            .build();
-
+    private class ViewHolder{
+        TextView tvTitle;
+        TextView tvHost;
+        TextView tvMembers;
+        TextView tvAdmires;
+        TextView tvLbs;
+        ImageView ivAvatar;
+    }
 
     public LiveShowAdapter(Activity activity, int resource, ArrayList<LiveInfoJson> objects) {
         super(activity, resource, objects);
@@ -52,31 +49,42 @@ public class LiveShowAdapter extends ArrayAdapter<LiveInfoJson> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView != null) {
-            view = convertView;
-        } else {
-            view = LayoutInflater.from(getContext()).inflate(R.layout.item_liveshow, null);
-        }
-        LiveInfoJson data = getItem(position);
+        ViewHolder holder;
 
-        ImageView avatar = (ImageView) view.findViewById(R.id.avatar);
+        if (convertView != null) {
+            holder = (ViewHolder)convertView.getTag();
+        } else {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_liveshow, null);
+
+            holder = new ViewHolder();
+            holder.tvTitle = (TextView) convertView.findViewById(R.id.live_title);
+            holder.tvHost = (TextView) convertView.findViewById(R.id.host_name);
+            holder.tvMembers = (TextView) convertView.findViewById(R.id.live_members);
+            holder.tvAdmires = (TextView) convertView.findViewById(R.id.praises);
+            holder.tvLbs = (TextView) convertView.findViewById(R.id.live_lbs);
+            holder.ivAvatar = (ImageView) convertView.findViewById(R.id.avatar);
+
+            convertView.setTag(holder);
+        }
+
+        LiveInfoJson data = getItem(position);
         if (null == data.getHost() || TextUtils.isEmpty(data.getHost().getAvatar())){
             // 显示默认图片
             Bitmap bitmap = BitmapFactory.decodeResource(this.getContext().getResources(), R.drawable.default_avatar);
             Bitmap cirBitMap = UIUtils.createCircleImage(bitmap, 0);
-            avatar.setImageBitmap(cirBitMap);
+            holder.ivAvatar.setImageBitmap(cirBitMap);
         }else{
             Log.d(TAG, "user avator: " + data.getHost().getAvatar());
             RequestManager req = Glide.with(mActivity);
-            req.load(data.getHost().getAvatar()).transform(new GlideCircleTransform(mActivity)).into(avatar);
+            req.load(data.getHost().getAvatar()).transform(new GlideCircleTransform(mActivity)).into(holder.ivAvatar);
         }
 
-        TextView hostName = (TextView) view.findViewById(R.id.host_name);
-        hostName.setText("@"+data.getHost().getUid());
-        TextView title = (TextView) view.findViewById(R.id.live_title);
-        title.setText(UIUtils.getLimitString(data.getTitle(), 10));
-        TextView admire = (TextView) view.findViewById(R.id.praises);
-        admire.setText("" + data.getAdmireCount());
-        return view;
+        holder.tvTitle.setText(UIUtils.getLimitString(data.getTitle(), 10));
+        holder.tvHost.setText("@" + data.getHost().getUid());
+        holder.tvMembers.setText(""+data.getWatchCount());
+        holder.tvAdmires.setText(""+data.getAdmireCount());
+        holder.tvLbs.setText(data.getLbs().getAddress());
+
+        return convertView;
     }
 }
